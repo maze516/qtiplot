845dfcd21b91a8574690d252669eba7022d30a7e
Félix Piédallu <salamandar@excalibur.home>
2016-03-23 11:25:32 +0100

    Image widget patch

--- qtiplot/src/plot2D/ImageWidget.h
+++ qtiplot/src/plot2D/ImageWidget.h
@@ -67,10 +67,10 @@ public:
 	void setWindowName(const QString& name){d_window_name = name;};
 
 	static QPixmap windowPixmap(ApplicationWindow *mw, const QString& name, const QSize& size);
+	void paintEvent(QPaintEvent *e);
 
 private:
 	void draw(QPainter *painter, const QRect& r);
-	void paintEvent(QPaintEvent *e);
 	QPixmap d_pix;
 	//! The file from which the image was loaded.
 	QString d_file_name;
